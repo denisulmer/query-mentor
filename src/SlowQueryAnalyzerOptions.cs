ce6bafdec2fb0966547b62310fdd7485f7f529a3
Denis Ulmer <github@denisulmer.dev>
2025-02-23 10:54:08 +0100

    Update SlowQueryAnalyzerOptions.cs

--- src/SlowQueryAnalyzerOptions.cs
+++ src/SlowQueryAnalyzerOptions.cs
@@ -2,5 +2,6 @@ public class SlowQueryAnalyzerOptions
 {
     public string AzureOpenAIEndpoint { get; set; }
     public string AzureOpenAIKey { get; set; }
+    public string ModelName { get; set; }
     public double SlowQueryThresholdMs { get; set; } = 500; // Default to 500ms
 }
